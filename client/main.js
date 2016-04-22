cb536160c85701378c93da23c75ba3e3fb6aa95c
Qilin Liu <565199219@qq.com>
2016-04-22 15:08:05 -0700

    add player movement

--- client/main.js
+++ client/main.js
@@ -5,6 +5,7 @@ window.Phaser = require('phaser/build/custom/phaser-split');
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var player;
+var cursors;
 
 function preload() {
     game.load.spritesheet('player', '/assets/player.png', 32, 16);
@@ -12,7 +13,28 @@ function preload() {
 
 function create() {
     player = game.add.sprite(500, 200, 'player');
+    game.physics.arcade.enable(player);
+    player.body.collideWorldBounds = true;
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+    player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -300;
+    }
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 300;
+    }
+    if (cursors.up.isDown)
+    {
+    	player.body.velocity.y = -300;
+    }
+    else if (cursors.down.isDown)
+    {
+    	player.body.velocity.y = 300;
+    }
 }
