547462a5004bc35079ce935f7b78f6e82732029a
Christopher Banh <chris@chrisbanh.com>
2016-04-22 15:31:02 -0700

    Add rabbit sprite

--- client/main.js
+++ client/main.js
@@ -14,9 +14,11 @@ var level;
 var levelText;
 var exp;
 var expText;
+var rabbit;
 
 function preload() {
     game.load.spritesheet('player', '/assets/player.png', 32, 16);
+    game.load.spritesheet('rabbit', '/assets/rabbit.png', 32, 32);
 }
 
 function create() {
@@ -28,9 +30,11 @@ function create() {
     manaText = game.add.text(16, 48, 'Mana: 100', { fontSize: '16px', fill: '#670' });
     expText = game.add.text(16, 64, 'Exp: 0', {fontSize: '16px', fill: '#670'});
     cursors = game.input.keyboard.createCursorKeys();
+    rabbit = game.add.sprite(200, 200, 'rabbit');
 }
 
 function update() {
+	rabbit.frame = 5;
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
     if (cursors.left.isDown)
