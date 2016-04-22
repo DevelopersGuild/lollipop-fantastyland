1b003a0c269be5ffc40a08464f197e835600ed17
Christopher Banh <chris@chrisbanh.com>
2016-04-22 12:51:43 -0700

    Add gulp and refactor app

--- client/main.js
+++ client/main.js
@@ -1,9 +1,13 @@
+window.PIXI = require('phaser/build/custom/pixi');
+window.p2 = require('phaser/build/custom/p2');
+window.Phaser = require('phaser/build/custom/phaser-split');
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var player;
 
 function preload() {
-    game.load.spritesheet('player', '../assets/player.png', 32, 16);
+    game.load.spritesheet('player', '/assets/player.png', 32, 16);
 }
 
 function create() {
---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+
+var browserify = require('browserify');
+var browserSync = require('browser-sync');
+var buffer = require('vinyl-buffer');
+var gulp = require('gulp');
+var gulpif = require('gulp-if');
+var nodemon = require('nodemon');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+
+gulp.task('compile-js', function() {
+  return browserify('client/main.js')
+    .bundle()
+    .pipe(source('scripts.min.js'))
+    .pipe(buffer())
+    .pipe(gulpif(process.env.NODE_ENV === 'production', uglify()))
+    .pipe(gulp.dest('public'))
+  }
+);
+
+gulp.task('server', ['compile-js'], function() {
+  browserSync.create().init({
+    files: ['public/**'],
+    port: 3001,
+    proxy: 'http://localhost:3000',
+    ui: { port: 3002 },
+    open: false,
+  });
+
+  gulp.watch(['client/*.js'], ['compile-js']);
+
+  return nodemon({
+    script: 'server/index.js',
+    env: { 'NODE_ENV': 'development' },
+    watch: ['server/**/*'],
+  });
+});
