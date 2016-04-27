26c453fa9675f3a19128b1cf74bdf85a2f183c20
Yang Chin <fyang96c@gmail.com>
2016-04-26 23:15:06 -0700

    Added kill enemy function, exp level system

--- client/main.js
+++ client/main.js
@@ -14,6 +14,7 @@ var levelText;
 var exp;
 var expText;
 var rabbit;
+var rabbitHp;
 
 function preload() {
     game.load.spritesheet('player', '/assets/player.png', 32, 16);
@@ -23,6 +24,7 @@ function preload() {
 function create() {
     player = game.add.sprite(500, 200, 'player');
     player.health = 100;
+    exp = 0;
     game.physics.arcade.enable(player);
     player.body.collideWorldBounds = true;
     levelText = game.add.text(16, 16, 'level: 1', { fontSize: '16px', fill: '#670'});
@@ -31,11 +33,15 @@ function create() {
     expText = game.add.text(16, 64, 'Exp: 0', {fontSize: '16px', fill: '#670'});
     cursors = game.input.keyboard.createCursorKeys();
     rabbit = game.add.sprite(200, 200, 'rabbit');
+    game.physics.arcade.enable(rabbit);
+    rabbit.body.collideWorldBounds = true;
+    rabbitHp = 3;
 }
 
 function update() {
 	rabbit.frame = 5;
 	healthText.text = 'Health: ' + player.health;
+	expText.text = 'Exp: ' + exp;
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
     if (cursors.left.isDown)
@@ -54,4 +60,30 @@ function update() {
     {
     	player.body.velocity.y = 300;
     }
+
+
+    //game.physics.arcade.collide(player, rabbit);
+    // run collision
+
+    game.physics.arcade.overlap(player, rabbit, killEnemy, null, this);
+}
+
+var hitText;
+
+// Collide with rabbit twice to kill rabbit.s
+function killEnemy(player, rabbit){
+	rabbitHp--;
+	// Show text when hit
+	hitText = game.add.text(rabbit.body.x, rabbit.body.y, "Ouch!", { fontSize: '16px', fill: '#670'});
+	// Kill enemy if enemyHp is 0.
+	if(rabbitHp == 0){
+		rabbit.kill();
+		exp += 10;	// Increment exp value
+		// Level up if exp reaches over 100.
+		if(exp >= 100){
+			level++;
+			exp = 0;
+		}
+	}
+	
 }
