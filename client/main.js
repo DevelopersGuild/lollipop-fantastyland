7cde56465fa0f095daa9c95942b33947aff048a0
Qilin Liu <565199219@qq.com>
2016-05-01 21:12:01 -0700

    added some fun stuff :P
    - now use wasd to move instead of arrow keys
    - now the player will accelerate to the max velocity instead of gain
    the velocity instantly
    - when the player hits the rabbit, both of them will receive damage,
    and the player will be knocked back
    - the rabbit will respawn when killed
    - when the player is killed, the game is over
    - when the left key of the mouse is held, the player will shoot bullets
    toward the cursor, but they can’t do anything yet
    - and some minor changes

--- client/main.js
+++ client/main.js
@@ -7,13 +7,13 @@ var playerModule = require('./modules/player');
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 var healthText;
-var mana;
 var manaText;
-var level;
 var levelText;
 var expText;
 var rabbit;
-var rabbitHp;
+var hitText;
+var levelUpText;
+var gameOverText;
 
 function preload() {
   // Modules
@@ -27,48 +27,74 @@ function create() {
   // Modules
   playerModule.create();
 
-  levelText = game.add.text(16, 16, 'level: 1', { fontSize: '16px', fill: '#670'});
+  levelText = game.add.text(16, 16, 'Level: 1', { fontSize: '16px', fill: '#670'});
   healthText = game.add.text(16, 32, 'Health: 100', { fontSize: '16px', fill: '#670' });
   manaText = game.add.text(16, 48, 'Mana: 100', { fontSize: '16px', fill: '#670' });
   expText = game.add.text(16, 64, 'Exp: 0', {fontSize: '16px', fill: '#670'});
-  cursors = game.input.keyboard.createCursorKeys();
   rabbit = game.add.sprite(200, 200, 'rabbit');
   game.physics.arcade.enable(rabbit);
   rabbit.body.collideWorldBounds = true;
-  rabbitHp = 3;
+  rabbit.health = 3;
 }
 
 function update() {
   // Modules
   playerModule.update();
 
-  rabbit.frame = 5;
+  rabbit.frame = 4;
 
   // TEMP
+  levelText.text = 'Level: ' + playerModule.getLevel();
   healthText.text = 'Health: ' + playerModule.getHealth();
   expText.text = 'Exp: ' + playerModule.getExp();
-
-  //game.physics.arcade.collide(player, rabbit);
-  // run collision
 
   game.physics.arcade.overlap(playerModule.getPlayer(), rabbit, killEnemy, null, this);
 }
 
-var hitText;
+function destroyText(text) {
+  setTimeout(function() {
+    text.destroy()
+  }, 1000);
+}
 
-// Collide with rabbit twice to kill rabbit.s
-function killEnemy(player, rabbit){
-  rabbitHp--;
-  // Show text when hit
-  hitText = game.add.text(rabbit.body.x, rabbit.body.y, "Ouch!", { fontSize: '16px', fill: '#670'});
+function killEnemy(player, rabbit) {
+  // player is knocked back
+  if (player.body.velocity.x > 50)
+    player.body.velocity.x = -200;
+  else if (player.body.velocity.x < -50)
+    player.body.velocity.x = 200;
+  else player.body.velocity.x = 0;
+  if (player.body.velocity.y > 50)
+    player.body.velocity.y = -200;
+  else if (player.body.velocity.y < -50)
+    player.body.velocity.y = 200;
+  else player.body.velocity.y = 0;
+  // deal damage to both of them
+  rabbit.health--;
+  player.health--;
+  // show text when hit
+  hitText = game.add.text(rabbit.x, rabbit.y, "-1", { fontSize: '16px', fill: 'red'});
+  destroyText(hitText);
+  hitText = game.add.text(player.x, player.y, "-1", { fontSize: '16px', fill: 'red'});
+  destroyText(hitText);
   // Kill enemy if enemyHp is 0.
-  if(rabbitHp == 0){
-    rabbit.kill();
+  if (rabbit.health == 0) {
     player.exp += 10;  // Increment exp value
     // Level up if exp reaches over 100.
-    if(player.exp >= 100){
-      level++;
+    if (player.exp >= 100) {
+      player.level++;
       player.exp = 0;
+      levelUpText = game.add.text(player.x, player.y - 50, "Level Up!", { fontSize: '16px', fill: 'yellow'});
+      destroyText(levelUpText);
     }
+    // rabbit respawns when killed
+    rabbit.x = Math.random() * 800;
+    rabbit.y = Math.random() * 600;
+    rabbit.health = 3;
+  }
+  // if player's health is 0, the game is over
+  if (player.health == 0) {
+    player.kill();
+    gameOverText = game.add.text(200, 250, "Game Over", { fontSize: '64px', fill: 'red'});
   }
 }
--- client/modules/player.js
+++ client/modules/player.js
@@ -1,6 +1,12 @@
-var cursors;
 var game;
 var player;
+var bullets;
+var fireRate = 300;
+var nextFire = 0;
+var w;
+var s;
+var a;
+var d;
 
 function initialize(_game) {
   game = _game;
@@ -8,6 +14,7 @@ function initialize(_game) {
 
 function preload() {
   game.load.spritesheet('player', '/assets/player.png', 32, 16);
+  game.load.image('bullet', '/assets/bullet.png');
 }
 
 function create() {
@@ -15,8 +22,21 @@ function create() {
   game.physics.arcade.enable(player);
   player.anchor.setTo(0.5, 0.5);
   player.body.collideWorldBounds = true;
+  player.body.allowRotation = false;
 
-  cursors = game.input.keyboard.createCursorKeys();
+  bullets = game.add.group();
+  bullets.enableBody = true;
+  bullets.createMultiple(50, 'bullet');
+  bullets.setAll('checkWorldBounds', true);
+  bullets.setAll('outOfBoundsKill', true);
+
+  w = game.input.keyboard.addKey(Phaser.Keyboard.W);
+  a = game.input.keyboard.addKey(Phaser.Keyboard.A);
+  s = game.input.keyboard.addKey(Phaser.Keyboard.S);
+  d = game.input.keyboard.addKey(Phaser.Keyboard.D);
+
+  // Initialize Level
+  player.level = 1;
 
   // Initialize Health
   player.health = 100;
@@ -26,24 +46,36 @@ function create() {
 }
 
 function update() {
-  player.body.velocity.x = 0;
-  player.body.velocity.y = 0;
-  if (cursors.left.isDown)
-  {
-      player.body.velocity.x = -300;
+  if (player.body.velocity.x > 0)
+      player.body.acceleration.x = -300;
+  if (player.body.velocity.x < 0)
+      player.body.acceleration.x = 300;
+  if (player.body.velocity.y > 0)
+      player.body.acceleration.y = -300;
+  if (player.body.velocity.y < 0)
+      player.body.acceleration.y = 300;
+
+  player.body.maxVelocity.x = 200;
+  player.body.maxVelocity.y = 200;
+  player.rotation = game.physics.arcade.angleToPointer(player);
+  if (game.input.activePointer.isDown) fire();
+
+  if (a.isDown) {
+      player.body.acceleration.x = -500;
   }
-  else if (cursors.right.isDown)
-  {
-      player.body.velocity.x = 300;
+  else if (d.isDown) {
+      player.body.acceleration.x = 500;
   }
-  if (cursors.up.isDown)
-  {
-    player.body.velocity.y = -300;
+  if (w.isDown) {
+      player.body.acceleration.y = -500;
   }
-  else if (cursors.down.isDown)
-  {
-    player.body.velocity.y = 300;
+  else if (s.isDown) {
+      player.body.acceleration.y = 500;
   }
+}
+
+function getLevel() {
+  return player.level;
 }
 
 function getHealth() {
@@ -58,11 +90,21 @@ function getPlayer() {
   return player;
 }
 
+function fire() {
+  if (game.time.now > nextFire && bullets.countDead() > 0) {
+    nextFire = game.time.now + fireRate;
+    var bullet = bullets.getFirstDead();
+    bullet.reset(player.x, player.y);
+    game.physics.arcade.moveToPointer(bullet, 300);
+  }
+}
+
 module.exports = {
   initialize: initialize,
   preload: preload,
   create: create,
   update: update,
+  getLevel: getLevel,
   getHealth: getHealth,
   getExp: getExp,
   getPlayer: getPlayer,
