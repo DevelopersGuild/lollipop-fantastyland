6cff007388103e19ea971eabdcfc4621a360af1c
Christopher Banh <chris@chrisbanh.com>
2016-05-20 10:22:01 -0700

    Redesign grassland1

--- client/modules/level.js
+++ client/modules/level.js
@@ -4,22 +4,24 @@ const module = {
   preload(game) {
     this.game = game;
     this.game.load.tilemap('map', 'assets/grassland1.json', null, Phaser.Tilemap.TILED_JSON);
-    this.game.load.image('grass', 'assets/tilesets/grass-tiles-2-small.png');
-    this.game.load.image('tree', 'assets/tilesets/tree2-final.png');
+    this.game.load.image('browserquest', 'assets/tilesets/browserquest.png');
   },
 
   create() {
     this.map = this.game.add.tilemap('map');
-    this.map.addTilesetImage('grass-tiles-2-small', 'grass');
-    this.map.addTilesetImage('tree2-final', 'tree');
+    this.map.addTilesetImage('browserquest', 'browserquest');
 
-    this.backgroundLayer = this.map.createLayer('Background');
-    this.foregroundLayer = this.map.createLayer('Foreground');
+    this.background1Layer = this.map.createLayer('Background-1');
+    this.background2Layer = this.map.createLayer('Background-2');
+    this.foregroundLayer = this.map.createLayer('Foreground-Pass');
+    this.collisionLayer = this.map.createLayer('Foreground-Collide');
 
-    this.backgroundLayer.resizeWorld();
+    this.background1Layer.resizeWorld();
+    this.background2Layer.resizeWorld();
     this.foregroundLayer.resizeWorld();
-    this.game.physics.arcade.enable(this.foregroundLayer);
-    this.map.setCollisionByExclusion([], true, this.foregroundLayer);
+    this.collisionLayer.resizeWorld();
+    this.game.physics.arcade.enable(this.collisionLayer);
+    this.map.setCollisionByExclusion([], true, this.collisionLayer);
   },
 
   createTopLayer() {
@@ -28,9 +30,9 @@ const module = {
   },
 
   update(player, rabbit, slime) {
-    this.game.physics.arcade.collide(player, this.foregroundLayer);
-    this.game.physics.arcade.collide(rabbit, this.foregroundLayer);
-    this.game.physics.arcade.collide(slime, this.foregroundLayer);
+    this.game.physics.arcade.collide(player, this.collisionLayer);
+    this.game.physics.arcade.collide(rabbit, this.collisionLayer);
+    this.game.physics.arcade.collide(slime, this.collisionLayer);
   },
 };
 
