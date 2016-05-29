2cab266f4a810593c0667f49e85b140750a9e714
Christopher Banh <chris@chrisbanh.com>
2016-05-28 23:56:20 -0700

    Add more enemies, and fix some bugs with player module

--- client/modules/player.js
+++ client/modules/player.js
@@ -1,4 +1,9 @@
 
+function destroyText(text) {
+  setTimeout(() => {
+    text.destroy();
+  }, 1000);
+}
 
 const module = {
   preload(game) {
@@ -77,12 +82,12 @@ const module = {
     if (this.player.exp >= 100) {
       this.player.level++;
       this.player.exp -= 100;
-      this.levelUpText = game.add.text(this.player.x, this.player.y - 50, 'Level Up!', { fontSize: '16px', fill: 'yellow' });
-      this.destroyText(this.levelUpText);
+      this.levelUpText = this.game.add.text(this.player.x, this.player.y - 50, 'Level Up!', { fontSize: '16px', fill: 'yellow' });
+      destroyText(this.levelUpText);
     }
     if (this.player.health === 0) {
       this.player.kill();
-      this.gameOverText = game.add.text(200, 250, "Game Over", { fontSize: '64px', fill: 'red'});
+      this.gameOverText = this.game.add.text(200, 250, "Game Over", { fontSize: '64px', fill: 'red'});
     }
   },
 
